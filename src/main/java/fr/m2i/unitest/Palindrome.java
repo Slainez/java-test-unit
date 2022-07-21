6234e7e11d33dffe183e07708dfbbcc2235bf011
Slainez <sullyvan360@gmail.com>
2022-07-21 16:00:50 +0200

    exercice-palindrome

---
+++ src/main/java/fr/m2i/unitest/Palindrome.java
@@ -0,0 +1,20 @@
+
+package fr.m2i.unitest;
+
+
+public class Palindrome {
+    
+    static boolean isPalindrome(String chaine){        
+        chaine.replaceAll(" ","" );
+        String chaineInvert = "";        
+        for (int i = chaine.length() - 1; i>= 0 ; i--){
+           chaineInvert += chaine.charAt(i) ;           
+        }        
+        boolean palindrome = chaine.equals(chaineInvert) ;               
+        return palindrome ;
+    }
+    public static void main(String [] args){
+        System.out.println(isPalindrome("esope reste ici et se repose"));
+        
+    }
+}
